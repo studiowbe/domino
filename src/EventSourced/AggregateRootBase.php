d866d089b6da8624a99d53bc8c752093c2cda893
Willem Wollebrants <willem@studiow.be>
2022-07-21 23:38:47 +0200

    move AggregateRoot behaviour to trait

--- src/EventSourced/AggregateRootBase.php
+++ src/EventSourced/AggregateRootBase.php
@@ -2,38 +2,7 @@
 
 namespace Studiow\Domino\EventSourced;
 
-use Studiow\Domino\Event\EventStream;
-use Studiow\Domino\Event\RecordedEvent;
-
 abstract class AggregateRootBase implements AggregateRoot
 {
-    private $recordedEvents = [];
-
-    public function getRecordedEvents(): EventStream
-    {
-        return  new EventStream($this->recordedEvents);
-    }
-
-    public function clearRecordedEvents(): static
-    {
-        $this->recordedEvents = [];
-
-        return  $this;
-    }
-
-    public function event(object $event): static
-    {
-        $this->recordedEvents[] = RecordedEvent::now($this->getId(), $event);
-        $handler = [$this, $this->getEventHandlerMethodName($event)];
-        if (is_callable($handler)) {
-            call_user_func($handler, $event);
-        }
-
-        return  $this;
-    }
-
-    protected function getEventHandlerMethodName(object $event):string
-    {
-        return 'handle'.substr(strrchr(get_class($event), '\\'), 1);
-    }
+    use AggregateRootBehaviour;
 }
---
+++ src/EventSourced/AggregateRootBehaviour.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Studiow\Domino\EventSourced;
+
+use Studiow\Domino\Event\EventStream;
+use Studiow\Domino\Event\RecordedEvent;
+
+trait AggregateRootBehaviour
+{
+    private $recordedEvents = [];
+
+    public function getRecordedEvents(): EventStream
+    {
+        return  new EventStream($this->recordedEvents);
+    }
+
+    public function clearRecordedEvents(): static
+    {
+        $this->recordedEvents = [];
+
+        return  $this;
+    }
+
+    public function event(object $event): static
+    {
+        $this->recordedEvents[] = RecordedEvent::now($this->getId(), $event);
+        $handler = [$this, $this->getEventHandlerMethodName($event)];
+        if (is_callable($handler)) {
+            call_user_func($handler, $event);
+        }
+
+        return  $this;
+    }
+
+    protected function getEventHandlerMethodName(object $event):string
+    {
+        return 'handle'.substr(strrchr(get_class($event), '\\'), 1);
+    }
+}
